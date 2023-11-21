53df065842fe633cc702833ce1268a1b5f2538b1
Rottab <totundberuhmt@gmail.com>
2023-11-21 14:04:22 +0200

    Fixed a warning

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from "react";
 import { useStateValue } from "../StateProvider";
 import Button from "@mui/material/Button";
 
