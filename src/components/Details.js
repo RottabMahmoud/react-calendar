d7fc7b6a8b31525e5c24597d4455e35fc1ac564d
Rottab <totundberuhmt@gmail.com>
2023-12-01 13:15:27 +0200

    Added Some Boostrap Modal Stlyings and Imports.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Button } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import moment from "moment";
 import Datetime from "react-datetime";
 import "../css/datetime.css";
